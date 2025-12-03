a9e9bc1e3ba83bad1bc945ec285b272287606b3d
SunnyFGC <njluna21@yahoo.com>
2025-12-03 11:51:18 -0800

    Implemented Expose Helper Grid Methods

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 
 struct Pair {
@@ -13,23 +14,53 @@ struct Grid {
 
     // Expose helper functions
     bool inBounds(int row, int col) {
-
+        return row >= 0 &&
+               row < height &&
+               col >= 0 &&
+               col < width;
     }
 
 
-    bool isBlocked(int row, int col) {
-
-    }
-
+    bool isBlocked(int row, int col) { return cells[toID(row, col)] == '#'; }
 
     int toID(int row, int col) { return row * width + col; }
-
 
     Pair fromID(int id) { return {id / width, id % width}; }
 
 
     std::vector<int> getNeighbors(int id) {
+        std::vector<int> neighbors;
+        Pair pair = fromID(id);
 
+        // Up neighbor
+        Pair upNeighbor = {pair.row - 1, pair.col};
+        if (inBounds(upNeighbor.row, upNeighbor.col) &&
+            !isBlocked(upNeighbor.row, upNeighbor.col)) {
+            neighbors.push_back(toID(upNeighbor.row, upNeighbor.col));
+        }
+
+        // Down neighbor
+        Pair downNeighbor = {pair.row + 1, pair.col};
+        if (inBounds(downNeighbor.row, downNeighbor.col) &&
+            !isBlocked(downNeighbor.row, downNeighbor.col)) {
+            neighbors.push_back(toID(downNeighbor.row, downNeighbor.col));
+        }
+
+        // Left neighbor
+        Pair leftNeighbor = {pair.row, pair.col - 1};
+        if (inBounds(leftNeighbor.row, leftNeighbor.col) &&
+            !isBlocked(leftNeighbor.row, leftNeighbor.col)) {
+            neighbors.push_back(toID(leftNeighbor.row, leftNeighbor.col));
+        }
+
+        // Right neighbor
+        Pair rightNeighbor = {pair.row, pair.col + 1};
+        if (inBounds(rightNeighbor.row, rightNeighbor.col) &&
+            !isBlocked(rightNeighbor.row, rightNeighbor.col)) {
+            neighbors.push_back(toID(rightNeighbor.row, rightNeighbor.col));
+        }
+
+        return neighbors;
     }
 };
 
