40b4d84c76f5047420a204a8393243324746d0d3
Nathan <163361000+njmoon21@users.noreply.github.com>
2025-12-13 21:58:24 -0800

    Implemented BFS, File Loading, and Added 'sample.txt'

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <string>
+#include <fstream>
 #include <sstream>
+#include <algorithm>
 
 
 struct Pair {
@@ -72,6 +75,45 @@ enum class Algorithm {
 };
 
 
+void mapLoading(std::string& path, Grid& grid) {
+    std::ifstream file(path);
+
+    std::string line;
+    std::vector<std::string> gridLines;
+
+    // Reading map format configuration
+    while (std::getline(file, line)) {
+        if (line.empty() == true)
+            continue;
+
+        std::stringstream ss(line);
+        std::string config;
+        ss >> config;
+
+        if (config == "WIDTH")
+            ss >> grid.width;
+        else if (config == "HEIGHT")
+            ss >> grid.height;
+        else if (config == "START")
+            ss >> grid.start_row >> grid.start_col;
+        else if (config == "GOAL")
+            ss >> grid.goal_row >> grid.goal_col;
+        else
+            gridLines.push_back(line);
+    }
+
+    // Loading the grid from the file
+    grid.cells.resize(grid.width * grid.height);
+
+    for (int row = 0; row < grid.height; row++) {
+        for (int col = 0; col < grid.width; col++) {
+            char ch = gridLines[row][col];
+            grid.cells[grid.toID(row, col)] = ch;
+        }
+    }
+}
+
+
 struct argumentConfigs {
     bool map_mode_ = false, generation_mode_ = false;
     std::string map_path_;
@@ -101,7 +143,9 @@ bool argumentParser(int argc, char* argv[], argumentConfigs& configuration) {
         }
 
 
-        // If argv[i] == "--generate"
+        /*
+
+    // If argv[i] == "--generate"
         else if (arg == "--generate") {
             if (configuration.map_mode_ == true) {
                 std::cout << "Error: Map mode is already on.\n";
@@ -109,6 +153,9 @@ bool argumentParser(int argc, char* argv[], argumentConfigs& configuration) {
             }
             configuration.generation_mode_ = true;
         }
+
+         */
+
 
 
         // If argv[i] == "--algo"
@@ -198,11 +245,118 @@ bool argumentParser(int argc, char* argv[], argumentConfigs& configuration) {
     return true;
 }
 
+// Algorithm implementations
+struct algorithmSearch {
+    bool found = false;
+    std::vector<bool> visited;
+    std::vector<int> path; // Each int is an ID that a cell represents
+
+    // Step 7 is now optional
+    // int visited_amount_, path_len_, cost_, time;
+};
+
+
+algorithmSearch BFS(Grid& grid) {
+    algorithmSearch result;
+
+    int n = grid.width * grid.height; // represents the amount of cells in the grid
+    result.visited.assign(n, false); // fill every cell with false, since at the start we haven't visited anything
+    std::vector<int> parents(n, -1); // Each cell containing a parentID, -1 is a sentinel value
+
+    int startId = grid.toID(grid.start_row, grid.start_col);
+    int goalId = grid.toID(grid.goal_row, grid.goal_col);
+
+
+    // "BFS uses a queue and guarantees shortest paths on unit grids"
+    // Start
+    std::queue<int> queue;
+    queue.push(startId);
+    result.visited[startId] = true;
+
+
+    while (!queue.empty()) {
+        int curr = queue.front();
+        queue.pop();
+
+        if (curr == goalId) {
+            result.found = true;
+            break;
+        }
+
+        for (int neighbor : grid.getNeighbors(curr)) {
+            if (result.visited[neighbor] == false) {
+                result.visited[neighbor] = true;
+                parents[neighbor] = curr;
+                queue.push(neighbor);
+            }
+        }
+    }
+
+
+    if (result.found == true) {
+        int curr = goalId;
+        while (curr != -1) {
+            result.path.push_back(curr);
+            curr = parents[curr];
+        }
+        std::reverse(result.path.begin(), result.path.end());
+    }
+
+
+    return result;
+}
+
+
+algorithmSearch Dijkstras(Grid& grid) {
+
+}
+
+algorithmSearch Astar(Grid& grid) {
+
+}
+
+
+void print(Grid& grid, algorithmSearch& result, std::string& algorithm) {
+    std::vector<char> visualGrid = grid.cells;
+
+
+    for (int i = 0; i < result.visited.size(); i++) {
+        if (result.visited[i] && visualGrid[i] == '.')
+            visualGrid[i] = '+';
+    }
+
+    if (result.found == true) {
+        for (int cellId : result.path) {
+            if (visualGrid[cellId] != '#')
+                visualGrid[cellId] = '*';
+        }
+    }
+
+
+    int startId = grid.toID(grid.start_row, grid.start_col);
+    int goalId = grid.toID(grid.goal_row, grid.goal_col);
+
+    visualGrid[startId] = 'S';
+    visualGrid[goalId] = 'G';
+
+
+    // Final print
+    std::cout << "=== " << algorithm << " ===\n";
+    for (int row = 0; row < grid.height; row++) {
+        for (int col = 0; col < grid.width; col++) {
+            std::cout << visualGrid[grid.toID(row, col)];
+        }
+        std::cout << "\n";
+    }
+    bool pathFound = result.found;
+    std::cout << "Found: " << pathFound << "\n";
+}
+
 
 int main(int argc, char* argv[]) {
     if (argc != 5 && argc != 10) {
         std::cout << "Invalid argument count. Please try again\n";
-        return -1;
+        return 1;
     }
 
     argumentConfigs configuration;
@@ -210,7 +364,7 @@ int main(int argc, char* argv[]) {
     if (parsingSuccessful == false)
         return 1;
 
-    // TODO 
+    // TODO
 
     return 0;
 }
